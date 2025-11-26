3dad4e746929f78019ffb15e77a594dd380496f5
Lucas Paixao <lucassernajoto8@gmail.com>
2025-11-25 21:46:05 -0300

    chore: adjust output sensitivity for Kubernetes cluster details in Terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,17 @@
 output "kubernetes_cluster_name" {
   value = module.gke.name
-  sensitive = true
+}
+
+output "kubernetes_cluster_location" {
+  value = module.gke.location
 }
 
 output "kubernetes_cluster_endpoint" {
-  value = module.gke.endpoint
+  value     = module.gke.endpoint
   sensitive = true
 }
 
 output "kubernetes_cluster_ca_certificate" {
   value     = module.gke.ca_certificate
-sensitive = true
+  sensitive = true
 }
