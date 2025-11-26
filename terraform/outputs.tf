07ec653483b63b532faedb812fe92d687f48f6d7
Lucas Paixao <lucassernajoto8@gmail.com>
2025-11-25 21:35:55 -0300

    chore: create deployment configuration and mark sensitive outputs in Terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,14 @@
 output "kubernetes_cluster_name" {
   value = module.gke.name
-  sensitive = false
+  sensitive = true
 }
 
 output "kubernetes_cluster_endpoint" {
   value = module.gke.endpoint
-  sensitive = false
+  sensitive = true
 }
 
 output "kubernetes_cluster_ca_certificate" {
   value     = module.gke.ca_certificate
-  sensitive = false
+sensitive = true
 }
