acc954f5a38e8db4b110603d4ec4d54de1ea1084
Lucas Paixao <lucassernajoto8@gmail.com>
2025-11-28 01:05:01 -0300

    chore: update Playwright workflow to trigger on pull requests instead of pushes

--- tests/e2e/collections.spec.ts
+++ tests/e2e/collections.spec.ts
@@ -6,13 +6,14 @@ async function safeGoto(page: Page, url: string, retries = 5, delay = 1000) {
     for (let i = 0; i < retries; i++) {
         try {
             await page.goto(url);
+            expect(true).toBe(false);
             return;
         } catch (e) {
             if (e.message.includes('ECONNREFUSED')) {
                 console.log(`Tentativa ${i + 1} falhou com ECONNREFUSED. Tentando novamente em ${delay}ms...`);
                 await page.waitForTimeout(delay);
             } else {
-                throw e; // erro não relacionado a conexão, rethrow
+                throw e; 
             }
         }
     }
@@ -21,29 +22,22 @@ async function safeGoto(page: Page, url: string, retries = 5, delay = 1000) {
 
 test('Realizando login como Admin e criando categoria', async ({ page }) => {
     
-    // Acessar a página de login
     await safeGoto(page, 'http://localhost:1337/admin/auth/login');
 
-    // Logando na conta
     await page.fill('input[name="email"]', 'admin@satc.edu.br');
     await page.fill('input[name="password"]', 'welcomeToStrapi123');
     await page.click('button[type="submit"] span:has-text("Login")');
 
-    // Entrando na página Content Manager
     await page.click('a:has(span:has-text("Content Manager"))');
 
-    // Entrando na tag categoria
     await page.click('a:has(div:has(span:has-text("Categoria")))');
 
-    // Criando uma categoria
     await page.click('a:has(span:has-text("Create new entry"))');
     await page.fill('input[name="name"]', faker.person.jobTitle());
     await page.click('button:has(span:has-text("Save"))');
 
-    // Deslogando da conta
     await page.click('span.sc-Qotzb.jopkZf.sc-dKREkF.kGPoCp');
     await page.click('span:has-text("Log out")');
     
-    // Verificar se voltou para a tela inicial com "Welcome to Strapi!"
     await expect(page.locator('text=Welcome to Strapi!')).toBeVisible();
 });
